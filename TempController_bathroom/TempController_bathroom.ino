ca3088e276cfbfee33f9e408fe0781fa0ed5a8de
Andrea Pittaro <pittaroa@dei.unipd.it>
2017-10-15 18:48:25 +0200

    Added power saving mode and battery report   * Now the arduinos sleeps as long as possible taking 1 measure     per minute (approximatively)   * Arduino can self check its battery status and report to ESP

--- TempController_bathroom/TempController_bathroom.ino
+++ TempController_bathroom/TempController_bathroom.ino
@@ -4,12 +4,22 @@
  * @comment This sketch, when ATtiny85 boots ask the RaspberryPi the temperature I set for my house bedroom and 
  * when asked by the RPI turns on or off. periodically it gives to rpi the temperature measured.
  */
+ 
+#include <Wire.h> //transmission library
+#include <math.h> //needed for (int) = round(float/decimal var);
+#include <EEPROM.h> // store values into memory. I need 4 bytes, ATMega328p has 1024 bytes
+#include <avr/sleep.h> // power save mode
+#include <avr/wdt.h> // watchdog
+
+#define WAIT 7 //cycles of 8 seconds
+ 
 //pin relay
 #define relay 0 
 #define FORCE_MANUAL 1
 #define TEMPERATURE 2
+
 //Transmission spec
-#define ARD_ID 0x01
+#define ARD_ID 0x50
 //Message parameters
 #define IN_HOME   0b10000000
 #define STATUS    0b01100000
@@ -17,27 +27,39 @@
 #define HEAT_COOL 0b01000000
 #define TEMP_MASK 0b00001111
 #define ZERO_TEMP 150
+
 //Temperature parameters
 #define sensorT A0
 #define CONV_TEMP 0.48828125 //LM35 converting from Analog input to celsius degrees
 
 //EEPROM addresses
-#define ADDR_TEMP   1 // address where temperature is set
-#define ADDR_STATUS 2 // address where the status is set (manual mode and cooling or heating is on)
+#define ADDR_ID 1
+#define ADDR_TEMP   2 // address where temperature is set
+#define ADDR_STATUS 3 // address where the status is set (manual mode and cooling or heating is on)
 
+volatile unsigned long counter = 0; //timer for sleeping
+const long InternalReferenceVoltage = 1062; // power voltage. Needed for temperature 
 uint8_t chosenTemp = 190; // default temperature, in integer, one decimal
 float curTemp;
-//store info in only one var [inHome,manualMode,coolOn,heatOn,0,0,0,0] in 0 the one not used
+
+//store info in only one var [inHome,manualMode,coolOn,heatOn,0,0,battery[2 bit] ] in 0 the one not used
 uint8_t status = 0b10000000; //default: in home and manual mode
+/*battery info are 
+ * 00 for Extremely low (< 3V)
+ * 01 for Low (3V<= batt < 3.3V)
+ * 10 for Normal (3.3V <= batt < 4V)
+ * 11 for High (batt >= 4V)
+ */
+
 bool forceManual=false;
-uint8_t countIter = 0;
 uint8_t histTemp[5]; //last 5 temperatures, which corresponds to the last 5 minutes approximatly
-#include <Wire.h> //transmission library
-#include <math.h> //needed for (int) = round(float/decimal var);
-#include <EEPROM.h> // store values into memory. I need 4 bytes, ATMega328p has 1024 bytes
 //Initializes the ricetransmitter
 void setup() {
+    analogReference (INTERNAL);//Internal AREF
+    EEPROM.put(ADDR_ID, ARD_ID);
+
     Wire.begin(ARD_ID); // the arduino is a slave. 
+    TWAR = (ARD_ID << 1) | 1;  // enable broadcasts to be received
     Wire.onReceive(receiveData);
     Wire.onRequest(sendData);
     pinMode(relay,OUTPUT);
@@ -50,6 +72,26 @@ void setup() {
 }
 
 void loop() {
+   // sleep for about 56s, then measures the temperature level
+  //sleep doesn't change the pin status (i.e. the relay stays HIGH or LOW while arduino is sleeping)
+  if(++counter >= WAIT){
+    //
+    byte old_ADCSRA = ADCSRA;
+    // disable ADC
+    ADCSRA = 0;  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
+    sleep_enable();
+    sleep_cpu();
+    sleep_disable();
+    ADCSRA = old_ADCSRA;      
+    counter=0;
+    // release TWI bus / I2C
+    TWCR = bit(TWEN) | bit(TWIE) | bit(TWEA) | bit(TWINT);
+    // turn it back on again
+    Wire.begin(ARD_ID); 
+    TWAR = (ARD_ID << 1) | 1;  // enable broadcasts to be received
+    getBandgap(); //every minute check the battery status
+   }
   forceManual = (digitalRead(FORCE_MANUAL)==HIGH); //check if I set in failsafe mode
   //enable temperature sensor (gives current)
   digitalWrite(TEMPERATURE,HIGH);
@@ -75,14 +117,29 @@ void loop() {
    else
          digitalWrite(relay,LOW);
   }
-   if(countIter == 50){
-    histTemp[4] = histTemp[3];histTemp[3] = histTemp[2];histTemp[2] = histTemp[1];
-    histTemp[0]=curTemp; //store this temperature
-    countIter=0;
-   }
-   else{
+  histTemp[4] = histTemp[3];histTemp[3] = histTemp[2];histTemp[2] = histTemp[1];
+  histTemp[0]=curTemp; //store this temperature
+    
+  // sleep for about 56s, then measures the temperature level
+  //sleep doesn't change the pin status (i.e. the relay stays HIGH or LOW while arduino is sleeping)
+  for(uint8_t sleep= 0; sleep < 7; sleep++ ){
     //
-    countIter++; 
+    byte old_ADCSRA = ADCSRA;
+    // disable ADC
+    ADCSRA = 0;  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
+    sleep_enable();
+    sleep_cpu();
+    sleep_disable();
+    ADCSRA = old_ADCSRA;      
+        
+    // release TWI bus / I2C
+    TWCR = bit(TWEN) | bit(TWIE) | bit(TWEA) | bit(TWINT);
+    
+    // turn it back on again
+    Wire.begin(ARD_ID); 
+    TWAR = (ARD_ID << 1) | 1;  // enable broadcasts to be received
+
    }
    EEPROM.put(ADDR_TEMP, chosenTemp); // update EEPROM, if changed, with the new chosen temperature 
    EEPROM.put(ADDR_STATUS, status);
@@ -102,8 +159,38 @@ void receiveData(int numB){
 void sendData(int numB){
   uint8_t data[6];
   data[0] = histTemp[0];data[1] = histTemp[1];data[2] = histTemp[2];
-  data[3] = histTemp[3];data[4] = histTemp[4];data[5] = status;
+  data[3] = histTemp[3];data[4] = histTemp[4];
+  data[5] = status;
   Wire.write(data,6);
 }
+
+/* Code courtesy of "Coding Badly" and "Retrolefty" from the Arduino forum
+ * results are Vcc * 10
+ * So for example, 5V would be 50.
+ * - Exploits temp results, saves results into "status" var and in EEPROM
+ */
+void getBandgap () 
+  {
+  // REFS0 : Selects AVcc external reference
+  // MUX3 MUX2 MUX1 : Selects 1.1V (VBG)  
+   ADMUX = bit (REFS0) | bit (MUX3) | bit (MUX2) | bit (MUX1);
+   ADCSRA |= bit( ADSC );  // start conversion
+   while (ADCSRA & bit (ADSC))
+     { }  // wait for conversion to complete
+   uint8_t batt = (((InternalReferenceVoltage * 1024) / ADC) + 5) / 100; // range [0;50] 
+   //encode the value
+   uint8_t codedBatteryStatus;
+   if(batt > 45) { codedBatteryStatus= 0b11; }
+   else if(batt > 33) { codedBatteryStatus= 0b10; }
+   else if(batt > 30) { codedBatteryStatus= 0b1; }
+   else { codedBatteryStatus= 0b0; }
+   //store the battery status
+   uint8_t tmpStatus = EEPROM.read(ADDR_STATUS);
+   if((tmpStatus & 0b11)!= codedBatteryStatus){ //update battery status
+      tmpStatus = tmpStatus & 0b11111100; //reset battery info, keep other info
+      status = tmpStatus | codedBatteryStatus; //update status variable
+      EEPROM.update(ADDR_STATUS,status); // store it on eeprom if changed
+   }
+  } // end of getBandgap
 
 
